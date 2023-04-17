c3469da49e00953013d552d017643d994edb176e
at2n19 <at2n19@soton.ac.uk>
2023-04-18 00:10:31 +0200

    Add visualise statistics initial content

--- pages/2_📈_Visualise_statistics.py
+++ pages/2_📈_Visualise_statistics.py
@@ -33,41 +33,6 @@ with open(EXERCISE_LIST_PATH, 'rb') as f:
 
 st.markdown(
     """
-    Record the weight you used for a given exercise.
+    Visualise the statistics about one or more exercises.
     """
 )
-
-user = st.selectbox('Who is the user?', ('<select>', 'Alberto', 'Giuseppe'))
-exercise = st.selectbox('What is the exercise?', exercise_list)
-repetitions = st.slider('How many reps did you do?', 1, 20, value=8)
-weight = st.number_input('What weight did you use (kg)?', min_value=0.0, max_value=200.0, value=50.0, step=0.25)
-password = st.text_input('Password')
-if st.button('Add weight'):
-    if user == '<select>' or exercise == '<select>' or password != st.secrets["PASSWORD"]:
-        if user == '<select>':
-            st.error('You need to select a user')
-        if exercise == '<select>':
-            st.error('You need to select an exercise')
-        if password != st.secrets["PASSWORD"]:
-            st.error('You need to type a correct password')
-
-    else:
-        datetime = datetime.now(pytz.timezone("Europe/Rome"))
-        date = "{0}-{1}-{2}".format(datetime.year, datetime.month, datetime.day)
-        d = {repetitions: {exercise: [(date, weight)]}}
-        file = "{0}Weights{1}.pickle".format(AWS_BUCKET, user)
-        if fs.exists(file):
-            with fs.open(file, 'rb') as f:
-                d = pickle.load(f)
-                if repetitions in d.keys():
-                    if exercise in d[repetitions].keys():
-                        d[repetitions][exercise].append((date, weight))
-                    else:
-                        d[repetitions][exercise] = [(date, weight)]
-                else:
-                    d[repetitions] = {exercise: [(date, weight)]}
-        else:
-            fs.touch(file)
-        with fs.open(file, 'wb') as f:
-            pickle.dump(d, f)
-        st.success('Weight added successfully!')
