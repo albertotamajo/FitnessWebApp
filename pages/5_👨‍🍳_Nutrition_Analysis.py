5618df60e9e83b56178c48889473136cd53539e3
at2n19 <at2n19@soton.ac.uk>
2023-05-02 09:36:51 +0200

    Grid of images

--- pages/5_👨‍🍳_Nutrition_Analysis.py
+++ pages/5_👨‍🍳_Nutrition_Analysis.py
@@ -37,7 +37,7 @@ st.markdown(
 )
 
 query = st.text_input("Write your food here. One food per line.", value="")
-count = st.number_input('Number of items searched', min_value=1, max_value=100, value=5, step=1)
+count = st.number_input('Number of items searched', min_value=1, max_value=100, value=30, step=1)
 if st.button('Find food'):
     if query == "":
         st.error('You need to write some food')
@@ -46,18 +46,20 @@ if st.button('Find food'):
         url = "https://it.openfoodfacts.org/cgi/search.pl?search_terms={0}&search_simple=1&json=1&action=process"
         search_result = requests.get(url.format(query)).json()
         products = search_result['products'][:count]
-        print(len(products))
-        images = []
-        with concurrent.futures.ThreadPoolExecutor(max_workers=len(products)) as executor:
-            future_to_url = (executor.submit(fetch_image, p, i) for i, p in enumerate(products))
-            for future in concurrent.futures.as_completed(future_to_url):
-                try:
-                    data = future.result()
-                except Exception as exc:
-                    data = str(type(exc))
-                finally:
-                    images.append(data)
-        images.sort()
-        images = [img for id, img in images]
-        index = image_select("Select food", images, return_value="index", use_container_width=False)
+        #select_image = image_select("Select food", [st.image(p["image_front_small_url"]) for p in products], return_value="index", use_container_width=False)
+        [st.image(p["image_front_small_url"]) for p in products if "image_front_small_url" in p.keys()]
+        # print(len(products))
+        # images = []
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=len(products)) as executor:
+        #     future_to_url = (executor.submit(fetch_image, p, i) for i, p in enumerate(products))
+        #     for future in concurrent.futures.as_completed(future_to_url):
+        #         try:
+        #             data = future.result()
+        #         except Exception as exc:
+        #             data = str(type(exc))
+        #         finally:
+        #             images.append(data)
+        # images.sort()
+        # images = [img for id, img in images]
+        # index = image_select("Select food", images, return_value="index", use_container_width=False)
 
