4bc567aa3a699a4fbb3c166ab3446bb5e4b3d7ba
at2n19 <at2n19@soton.ac.uk>
2023-08-26 19:56:37 +0200

    Fix meal planner bug

--- pages/7_👨‍🍳_Food_database.py
+++ pages/7_👨‍🍳_Food_database.py
@@ -24,13 +24,16 @@ st.markdown(
 
 
 with st.expander("""### Add food from openfoodfacts"""):
-    query = st.text_input("Write the name of the food", value="")
+    query = st.text_input("Write the name of the food", value="", key="foodQuery")
     query = "+".join(query.split())
     url = "https://it.openfoodfacts.org/cgi/search.pl?search_terms={0}&search_simple=1&json=1&action=process"
     search_result = requests.get(url.format(query)).json()
+    products = search_result['products'][:10]
+    img = image_select("Select food", [p["image_front_small_url"] for p in products if "image_front_small_url" in p],
+                       return_value="index", use_container_width=True)
 
 with st.expander("""### Add food manually"""):
-    food_name = st.text_input("Write the name of the food", value="")
+    food_name = st.text_input("Write the name of the food", value="", key="foodName")
     calories = st.number_input('Write the calories of this food (100gr)', min_value=int(0), max_value=int(1000), step=1)
     carbs = st.number_input('Write the carbs of this food (100gr)', min_value=int(0), max_value=int(1000), step=1)
     proteins = st.number_input('Write the proteins of this food (100gr)', min_value=int(0), max_value=int(1000), step=1)
