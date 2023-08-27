949210675620334ee4b3d3bac9303eac4ab65d85
at2n19 <at2n19@soton.ac.uk>
2023-08-27 16:15:38 +0200

    Fix meal planner bug

--- Home.py
+++ Home.py
@@ -3,8 +3,22 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
+
 authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
@@ -16,30 +30,11 @@ name, authentication_status, username = authenticator.login('Login', 'main')
 
 if authentication_status:
     authenticator.logout('Logout', 'main')
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    hide_st_style = """
-    <style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    </style>
-    """
-    st.markdown(hide_st_style, unsafe_allow_html=True)
-
-    AWS_BUCKET = "fitnessmanagement/"
 
     st.write("# Welcome to the Fitness Management Web App! 👋")
 
     st.sidebar.success("Select one of these functionalities.")
 
-    st.markdown(
-        """
-        This web app is used by Alberto Tamajo and Giuseppe Mistretta.
-    """
-    )
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
