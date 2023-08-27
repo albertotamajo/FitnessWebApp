665e996e4c8ef47841eb84babc9fbee869a6ca3d
at2n19 <at2n19@soton.ac.uk>
2023-08-27 16:34:13 +0200

    Fix meal planner bug

--- Home.py
+++ Home.py
@@ -26,7 +26,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login('Login', 'sidebar')
 
 if authentication_status:
     authenticator.logout('Logout', 'sidebar')
